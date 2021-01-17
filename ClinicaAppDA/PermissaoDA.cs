21f5e16005e374dc9944c7507561cdab8e03adea
BasilNight <luiswingmen@hotmail.com>
2021-01-17 17:13:46 +0000

    Push antes de mudança para .netcore oops

---
+++ ClinicaAppDA/PermissaoDA.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace ClinicaAppDA
+{
+    class PermissaoDA
+    {
+        #region Atributos
+        //Connection String para a nossa base de dados
+        private readonly string connectionString = "Server=speedwayrental.database.windows.net;Database=Clinica; User Id = admn; password=Mememaster101; Trusted_Connection=False; Encrypt=True;";
+        private SqlConnection connection;
+        private SqlCommand command;
+        #endregion
+    }
+}
---
+++ ClinicaAppDA/UtilizadorDA.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SqlClient;
+using Newtonsoft;
+
+namespace ClinicaAppDA
+{
+    public class UtilizadorDA
+    {
+        #region Atributos
+        //Connection String para a nossa base de dados
+        private readonly string connectionString = "Server=speedwayrental.database.windows.net;Database=Clinica; User Id = admn; password=Mememaster101; Trusted_Connection=False; Encrypt=True;";
+        private SqlConnection connection;
+        private SqlCommand command;
+        #endregion
+
+        #region Métodos
+
+        //get, insere, delete, bla bla
+
+
+
+
+        #endregion
+
+    }
+}
