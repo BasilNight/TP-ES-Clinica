1bbe00851b538e053ee4f19e2ffa571388b058a4
BasilNight <luiswingmen@hotmail.com>
2021-01-20 18:46:49 +0000

    Mais metodos adicionados

--- ClinicaAppDA/ConsultaDA.cs
+++ ClinicaAppDA/ConsultaDA.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Data.SqlClient;
+using ClinicaAppBO;
+using System.Data;
 
 
 namespace ClinicaAppDA
@@ -17,8 +19,198 @@ namespace ClinicaAppDA
 
         #region Métodos
 
+        /// <summary>
+        /// Metodo que insere uma consulta nova na base de dados
+        /// </summary>
+        /// <param name="novaConsulta"></param>
+        /// <returns></returns>
+        public bool InsereConsulta(Consulta novaConsulta)
+        {
+
+            connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+
+            }
+            catch
+            {
+                //Conexao falhou
+                connection.Close();
+                return false;
+            }
+            if (connection.State.ToString() == "Open")
+            {
+
+                //Construçao da query...             
+                string comando;
+
+                comando = "insert into Consulta (Data, Descricao, Estado, ID_Local, ID_Tratamento) values (@data, @desc, @estado, @idLocal, @idTrat);";
+
+                SqlCommand cmdins = new SqlCommand(comando, connection);
+
+                cmdins.Parameters.AddWithValue("@data", novaConsulta.Data);
+                cmdins.Parameters.AddWithValue("@desc", novaConsulta.Descricao);
+                cmdins.Parameters.AddWithValue("@estado", novaConsulta.Estado);
+                cmdins.Parameters.AddWithValue("@idLocal", novaConsulta.IdLocal);
+                cmdins.Parameters.AddWithValue("@idTrat", novaConsulta.IdTratamento);
 
 
+                //Executa a query
+                int res = cmdins.ExecuteNonQuery();
+                if (res > 0)
+                {
+                    connection.Close();
+                    return true;
+                }
+                else return false;
+            }
+            else return false;
+        }
+
+        /// <summary>
+        /// Metodo que devolve uma lista de todas as consultas registadas na base de dados
+        /// </summary>
+        /// <returns></returns>
+        public List<Consulta> GetAllConsultas()
+        {
+            SqlDataReader dataReader;
+            List<Consulta> listaConsultas = new List<Consulta>();
+            DataSet ds = new DataSet();
+            connection = new SqlConnection(connectionString);
+            Consulta consulta;
+
+            try
+            {
+                connection.Open();
+            }
+            catch
+            {
+                connection.Close();
+                return null;
+            }
+            if (connection.State.ToString() == "Open")
+            {
+
+                //Construçao da query
+                SqlCommand cmdins = new SqlCommand();
+                string comando;
+                cmdins.Connection = connection;
+
+                comando = "SELECT * FROM Utilizador";
+                cmdins.CommandText = comando;
+
+                dataReader = cmdins.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    consulta = new Consulta();
+
+                    consulta.ID = int.Parse(dataReader.GetValue(0).ToString());
+                    consulta.Data = DateTime.Parse(dataReader.GetValue(1).ToString());
+                    consulta.Descricao = dataReader.GetValue(2).ToString();
+                    consulta.Estado = int.Parse(dataReader.GetValue(3).ToString());
+                    consulta.IdLocal = int.Parse(dataReader.GetValue(5).ToString());
+                    consulta.IdTratamento = int.Parse(dataReader.GetValue(6).ToString());
+
+                    listaConsultas.Add(consulta);
+                }
+
+                connection.Close();
+                return listaConsultas;
+            }
+            else return null;
+        }
+
+        /// <summary>
+        /// Metodo que atualiza uma consulta registada na base de dados
+        /// </summary>
+        /// <param name="consulta"></param>
+        /// <returns></returns>
+        public bool UpdateConsulta(Consulta consulta)
+        {
+            connection = new SqlConnection(connectionString);
+            command = new SqlCommand();
+
+            try
+            {
+                connection.Open();
+            }
+            catch
+            {
+                connection.Close();
+                return false;
+            }
+            if (connection.State.ToString() == "Open")
+            {
+
+                //Construçao da query...             
+                string comando;
+
+                comando = "update Consulta set Data = @data, Descricao = @desc, Estado = @estado, ID_Local = @idLocal, ID_Tratamento = @idTrat, where ID_Consulta = @ID;";
+
+                SqlCommand cmdins = new SqlCommand(comando, connection);
+
+                cmdins.Parameters.AddWithValue("@data", consulta.Data);
+                cmdins.Parameters.AddWithValue("@desc", consulta.Descricao);
+                cmdins.Parameters.AddWithValue("@estado", consulta.Estado);
+                cmdins.Parameters.AddWithValue("@idLocal", consulta.IdLocal);
+                cmdins.Parameters.AddWithValue("@idTrat", consulta.IdTratamento);
+                cmdins.Parameters.AddWithValue("@ID", consulta.ID);
+
+
+                //Executa a query
+                int res = cmdins.ExecuteNonQuery();
+                if (res > 0)
+                {
+                    connection.Close();
+                    return true;
+                }
+                else return false;
+            }
+            else return false;
+        }
+
+        /// <summary>
+        /// Metodo que elimina uma consulta registada na Base de dados
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public bool DeleteConsulta(int id)
+        {
+            connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+
+            }
+            catch
+            {
+                //Conexao falhou
+                connection.Close();
+                return false;
+            }
+            if (connection.State.ToString() == "Open")
+            {
+                //Construçao da query             
+                string comando;
+
+                comando = "delete from Consulta where ID_Consulta = @ID;";
+                SqlCommand cmdins = new SqlCommand(comando, connection);
+                cmdins.Parameters.AddWithValue("@ID", id);
+
+                int res = cmdins.ExecuteNonQuery();
+                if (res > 0)
+                {
+                    connection.Close();
+                    return true;
+                }
+                else return false;
+            }
+            else return false;
+        }
 
 
         #endregion
--- ClinicaAppDA/UtilizadorDA.cs
+++ ClinicaAppDA/UtilizadorDA.cs
@@ -46,19 +46,21 @@ namespace ClinicaAppDA
             {
 
                 //Verificar se a pessoa ja existe
-                if (ExisteUtilizador(novoUtilizador.Username) == false)
+                if (ExisteUtilizador(novoUtilizador.Email) == false)
                 {
                     //Construçao da query...             
                     string comando;
 
-                    comando = ""; // FAZER A QUERY
+                    comando = "insert into Consulta (Data, Descricao, Estado, ID_Local, ID_Tratamento) values (@data, @desc, @estado, @idlocal, @idtrat);"; 
 
                     SqlCommand cmdins = new SqlCommand(comando, connection);
 
-                    cmdins.Parameters.AddWithValue(); // COMPLETAR COM PARAMETROS CORRETOS
-                    cmdins.Parameters.AddWithValue();
-                    cmdins.Parameters.AddWithValue();
-                    cmdins.Parameters.AddWithValue();
+                    cmdins.Parameters.AddWithValue("@data", novoUtilizador.Username); 
+                    cmdins.Parameters.AddWithValue("@desc", novoUtilizador.Email);
+                    cmdins.Parameters.AddWithValue("@estado", novoUtilizador.Senha);
+                    cmdins.Parameters.AddWithValue("@idlocal", novoUtilizador.DataNasc);
+                    cmdins.Parameters.AddWithValue("@idtrat", novoUtilizador.DataNasc);
+
 
 
                     //Executa a query
@@ -81,7 +83,7 @@ namespace ClinicaAppDA
         /// </summary>
         /// <param name="username"></param>
         /// <returns></returns>
-        public bool ExisteUtilizador(string username)
+        public bool ExisteUtilizador(string email)
         {
             connection = new SqlConnection(connectionString);
             DataTable dataTable = new DataTable();
@@ -101,7 +103,7 @@ namespace ClinicaAppDA
 
                 SqlCommand cmd = new SqlCommand(comando, connection);
 
-                cmd.Parameters.AddWithValue("", username); // TROCAR PARAMETROS
+                cmd.Parameters.AddWithValue("", email); // TROCAR PARAMETROS
 
                 SqlDataAdapter da = new SqlDataAdapter(cmd);
                 da.Fill(dataTable);
@@ -144,7 +146,7 @@ namespace ClinicaAppDA
                 string comando;
                 cmdins.Connection = connection;
 
-                comando = "SELECT * FROM pessoa";
+                comando = "SELECT * FROM Utilizador";
                 cmdins.CommandText = comando;
 
                 dataReader = cmdins.ExecuteReader();
@@ -193,17 +195,77 @@ namespace ClinicaAppDA
             if (connection.State.ToString() == "Open")
             {
                 //Verificar se a pessoa ja existe
-                if (ExisteUtilizador(utilizador.Username) == false)
+                if (ExisteUtilizador(utilizador.Email) == false)
                 {
+                    //Construçao da query...             
+                    string comando;
+
+                    comando = "update Utilizador set Username = @username, Senha = @senha, Data_Nascimento = @dataNasc, NIF = @nif, ID_Perfil = @idPerfil where [E-mail] = @email;";
+
+                    SqlCommand cmdins = new SqlCommand(comando, connection);
+
+                    cmdins.Parameters.AddWithValue("@username", utilizador.Username);
+                    cmdins.Parameters.AddWithValue("@email", utilizador.Email);
+                    cmdins.Parameters.AddWithValue("@senha", utilizador.Senha);
+                    cmdins.Parameters.AddWithValue("@dataNasc", utilizador.DataNasc);
+                    cmdins.Parameters.AddWithValue("@nif", utilizador.Nif);
+                    cmdins.Parameters.AddWithValue("@idPerfil", utilizador.IdPerfil);
 
 
-                    return true;
+                    //Executa a query
+                    int res = cmdins.ExecuteNonQuery();
+                    if (res > 0)
+                    {
+                        connection.Close();
+                        return true;
+                    }
+                    else return false;
+
                 }
                 else return false;
             }
             else return false;
         }
 
+        /// <summary>
+        /// Metodo que elimina utilizador registado na base de dados
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        public bool DeleteUtilizador(string email)
+        {
+            connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+
+            }
+            catch
+            {
+                //Conexao falhou
+                connection.Close();
+                return false;
+            }
+            if (connection.State.ToString() == "Open")
+            {
+                //Construçao da query             
+                string comando;
+
+                comando = "Delete from Utilizador where Utilizador.[E-mail] = @email;";
+                SqlCommand cmdins = new SqlCommand(comando, connection);
+                cmdins.Parameters.AddWithValue("@email", email);
+
+                int res = cmdins.ExecuteNonQuery();
+                if (res > 0)
+                {
+                    connection.Close();
+                    return true;
+                }
+                else return false;
+            }
+            else return false;
+        }
         #endregion
 
     }
